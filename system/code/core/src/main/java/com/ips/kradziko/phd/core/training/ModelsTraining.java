ebd4625b35542d81faf107f395d11285d6906f87
Kacper Radzikowski <radzikowskikacper@gmail.com>
2017-02-21 17:55:59 +0900

    init

--- system/code/core/src/main/java/com/ips/kradziko/phd/core/training/ModelsTraining.java
+++ system/code/core/src/main/java/com/ips/kradziko/phd/core/training/ModelsTraining.java
@@ -4,4 +4,9 @@ package com.ips.kradziko.phd.core.training;
  * Created by kradziko on 17/02/21.
  */
 public class ModelsTraining {
+    private String workdir;
+
+    private void generateBNFGrammar(String location){
+
+    }
 }

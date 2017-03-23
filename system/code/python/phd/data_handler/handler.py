733753bc099a7eab71a752c4ff092bcdff05d318
Kacper Radzikowski <radzikowskikacper@gmail.com>
2017-03-23 12:50:49 +0900

    changes

--- system/code/python/phd/data_handler/handler.py
+++ system/code/python/phd/data_handler/handler.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import os
 
 class handler:
     data = dict()
@@ -18,10 +19,38 @@ class handler:
                         self.data[parts[len(parts) - 1]] = list()
                     self.fnames[parts[len(parts) - 1]].append(parts[0])
 
-        for i in range(100):
-            print("hello")
-
         pass
 
+        list_of_files = []
+        for (dirpath, dirnames, filenames) in os.walk(data_dir + '/lbl/'):
+            for filename in filenames:
+                if filename == 'scores.lst':
+                    print(filename)
+                    parts = filename.split('/')
+                    gender = 'female' in parts[len(parts) - 3]
+                    pos = 2
+                    if 'segmental' in parts[len(parts) - 2]:
+                        pos = 0
+                    elif 'intonation' in parts[len(parts) - 2] or 'accent' in parts[len(parts) - 2]:
+                        pos = 1
+
+                    list_of_files.append(os.sep.join([dirpath, filename]))
+
+                    with open(os.sep.join([dirpath, filename])) as f:
+                        for current_line in f.readlines():
+                            if '#' in current_line:
+                                continue
+                            parts = current_line.split('\\s+')
+                            c = sum([int(x) for x in parts[1:]])
+                            c /= len(parts) - 1
+                            f = parts[0].split('/')[2]
+                            id = parts[0].split('/')[0] + '/' + parts[0].split('/')[1]
+
+                            for k, v in self.fnames.iteritems():
+                                if f in v:
+                                    for k2, v2 in self.data.iteritems():
+                                        
+
+        pass
     def transformData_Kaldi(self, output_dir):
         pass

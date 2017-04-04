74932fdaac1bdf73467678c772b6d0fe403fbec8
Kacper Radzikowski <radzikowskikacper@gmail.com>
2017-04-04 19:53:26 +0900

    code changes

--- system/code/python/phd/main.py
+++ system/code/python/phd/main.py
@@ -5,7 +5,7 @@ h = handler2()
 path = os.path.expanduser('~')
 
 if __name__ == '__main__':
-    h.load_data('{}/projects/research/phd/data'.format(path))
+    h.load_data('{}/projects/research/phd/data/ume-erj'.format(path))
 
     h.transformData_Kaldi_easier_tut('{}/projects/research/kaldi'.format(path),
                                      '{}/projects/research/phd/data/kaldi2'.format(path),

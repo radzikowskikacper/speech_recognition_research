ee4b8e8976f4c4d8b92b5003befa1111d34b6a64
Kacper Radzikowski <radzikowskikacper@gmail.com>
2017-04-25 20:05:41 +0900

    wine lda tests

--- system/code/python/phd/main.py
+++ system/code/python/phd/main.py
@@ -6,6 +6,9 @@ h = handler()
 path = os.path.expanduser('~')
 
 if __name__ == '__main__':
+    from testing import examples
+    examples.sim()
+    '''
     h.loader.load_data('{}/projects/research/phd/data/ume-erj'.format(path))
 
     h.formatter.transformData_Kaldi_easier_tut('{}/projects/research/kaldi'.format(path),
@@ -15,3 +18,4 @@ if __name__ == '__main__':
     h.formatter.prepare_training_script_Kaldi('{}/projects/research/kaldi'.format(path),
                                      '{}/projects/research/phd/data/kaldi'.format(path),
                                     '{}/projects/research/phd/data/kaldi/trainer.sh'.format(path))
+    '''
---
+++ system/code/python/phd/testing/examples.py
@@ -0,0 +1,40 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from matplotlib import colors
+
+def generate_data_set(n, d=1):
+    random.seed()
+    s = list()
+    for i in range(n):
+        s.append([random.randint(1, 10) for _ in range(d)])
+    return s
+
+def save_data(fname):
+    with open(fname, 'w') as f:
+        for l in generate_data_set(10, 2):
+            f.write(','.join([str(i) for i in l]) + '\n')
+
+def sim():
+    #save_data('test.data')
+    A = np.loadtxt('wine.data', delimiter=',')
+    y = A[:, 0]
+    # Remove targets from input data
+    A = A[:, 1:]
+    lda = LinearDiscriminantAnalysis(n_components=2)
+    lda.fit(A, y)
+    drA = lda.transform(A)
+
+    Z = generate_data_set(1, 13)
+    #Z = lda.transform(Z)
+    z_lab = lda.predict(Z)
+    z_prob = lda.predict_proba(Z)
+
+    plt.figure()
+    x = [l[0] for l in drA]
+    y = [l[1] for l in drA]
+    plt.scatter(x, y)
+    plt.legend()
+    plt.savefig('a.png')

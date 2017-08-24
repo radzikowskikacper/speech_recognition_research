d39351f717ecfb002e0fac87aeaa5f070b10037e
Kacper Radzikowski <radzikowskikacper@gmail.com>
2017-08-24 15:58:06 +0900

    fixes

--- code/main.py
+++ code/main.py
@@ -1,6 +1,6 @@
 from data_handler.handler import handler
 import os
-
+from testing import demo
 
 h = handler()
 path = os.path.expanduser('~')
@@ -11,8 +11,10 @@ if __name__ == '__main__':
     speaker_dependency.get_speaker_dependent_features()
     '''
 
-    from testing import examples
-    examples.sim()
+    #from testing import examples
+    #examples.sim()
+
+    demo.do_demo()
     
     '''
     h.loader.load_data('{}/projects/research/phd/data/ume-erj'.format(path))
--- code/testing/demo.py
+++ code/testing/demo.py
@@ -5,37 +5,38 @@ import tflearn
 
 from feature_extraction import speech_data
 
-learning_rate = 0.0001
-training_iters = 300000  # steps
-batch_size = 64
+def do_demo():
+    learning_rate = 0.0001
+    training_iters = 300000  # steps
+    batch_size = 64
 
-width = 20  # mfcc features
-height = 80  # (max) length of utterance
-classes = 10  # digits
+    width = 20  # mfcc features
+    height = 80  # (max) length of utterance
+    classes = 10  # digits
 
-batch = word_batch = speech_data.mfcc_batch_generator(batch_size)
+    batch = word_batch = speech_data.mfcc_batch_generator(batch_size)
 
-# Network building
-net = tflearn.input_data([None, width, height])
-net = tflearn.lstm(net, 128, dropout=0.8)
-net = tflearn.fully_connected(net, classes, activation='softmax')
-net = tflearn.regression(net, optimizer='adam', learning_rate=learning_rate, loss='categorical_crossentropy')
-# Training
+    # Network building
+    net = tflearn.input_data([None, width, height])
+    net = tflearn.lstm(net, 128, dropout=0.8)
+    net = tflearn.fully_connected(net, classes, activation='softmax')
+    net = tflearn.regression(net, optimizer='adam', learning_rate=learning_rate, loss='categorical_crossentropy')
+    # Training
 
-### add this "fix" for tensorflow version errors
-col = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
-for x in col:
-    tf.add_to_collection(tf.GraphKeys.VARIABLES, x )
+    ### add this "fix" for tensorflow version errors
+    col = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+    for x in col:
+        tf.add_to_collection(tf.GraphKeys.VARIABLES, x )
 
 
-model = tflearn.DNN(net, tensorboard_verbose=0)
-while 1: #training_iters
-    X, Y = next(batch)
-    trainX, trainY = X, Y
-    testX, testY = X, Y  # overfit for now
-    model.fit(trainX, trainY, n_epoch=10, validation_set=(testX, testY), show_metric=True,
-          batch_size=batch_size)
-    _y=model.predict(X)
-model.save("tflearn.lstm.model")
-print (_y)
-print (y)
+    model = tflearn.DNN(net, tensorboard_verbose=0)
+    while 1: #training_iters
+        X, Y = next(batch)
+        trainX, trainY = X, Y
+        testX, testY = X, Y  # overfit for now
+        model.fit(trainX, trainY, n_epoch=10, validation_set=(testX, testY), show_metric=True,
+              batch_size=batch_size)
+        _y=model.predict(X)
+    model.save("tflearn.lstm.model")
+    print (_y)
+    print (y)

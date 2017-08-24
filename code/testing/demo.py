3c7482991c422489feb55630ad26cd76f1b32886
Kacper Radzikowski <radzikowskikacper@gmail.com>
2017-08-24 16:03:27 +0900

    fixes

--- code/testing/demo.py
+++ code/testing/demo.py
@@ -15,6 +15,9 @@ def do_demo():
     classes = 10  # digits
 
     batch = word_batch = speech_data.mfcc_batch_generator(batch_size)
+    X, Y = next(batch)
+    trainX, trainY = X, Y
+    testX, testY = X, Y  # overfit for now
 
     # Network building
     net = tflearn.input_data([None, width, height])
@@ -26,17 +29,13 @@ def do_demo():
     ### add this "fix" for tensorflow version errors
     col = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
     for x in col:
-        tf.add_to_collection(tf.GraphKeys.VARIABLES, x )
-
+        tf.add_to_collection(tf.GraphKeys.VARIABLES, x)
 
     model = tflearn.DNN(net, tensorboard_verbose=0)
-    while 1: #training_iters
-        X, Y = next(batch)
-        trainX, trainY = X, Y
-        testX, testY = X, Y  # overfit for now
+    while 1:  # training_iters
         model.fit(trainX, trainY, n_epoch=10, validation_set=(testX, testY), show_metric=True,
-              batch_size=batch_size)
-        _y=model.predict(X)
+                  batch_size=batch_size)
+        _y = model.predict(X)
     model.save("tflearn.lstm.model")
-    print (_y)
-    print (y)
+    print(_y)
+    print(y)

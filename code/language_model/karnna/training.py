3737f687efb4f9c2b92ad922a3dd5ba27959c433
Kacper Radzikowski <radzikowskikacper@gmail.com>
2017-11-29 10:22:56 +0100

    fixes

--- code/language_model/karnna/training.py
+++ code/language_model/karnna/training.py
@@ -24,7 +24,7 @@ encoded = np.array([vocab_to_int[c] for c in text], dtype=np.int32)
 batches = data.get_batches(encoded, 10, 50)
 x, y = next(batches)
 
-network = model.create_model(len(vocab), batch_size=batch_size, num_steps=num_steps,
+initial_state, inputs, targets, keep_probs, losss, final_state, optimizer = model.create_model(len(vocab), batch_size=batch_size, num_steps=num_steps,
                 lstm_size=lstm_size, num_layers=num_layers,
                 learning_rate=learning_rate)
 
@@ -39,18 +39,18 @@ with tf.Session(config=config) as sess:
     counter = 0
     for e in range(epochs):
         # Train network
-        new_state = sess.run(model.initial_state)
+        new_state = sess.run(initial_state)
         loss = 0
         for x, y in data.get_batches(encoded, batch_size, num_steps):
             counter += 1
             start = time.time()
-            feed = {model.inputs: x,
-                    model.targets: y,
-                    model.keep_prob: keep_prob,
-                    model.initial_state: new_state}
-            batch_loss, new_state, _ = sess.run([model.loss,
-                                                 model.final_state,
-                                                 model.optimizer],
+            feed = {inputs: x,
+                    targets: y,
+                    keep_probs: keep_prob,
+                    initial_state: new_state}
+            batch_loss, new_state, _ = sess.run([losss,
+                                                 final_state,
+                                                 optimizer],
                                                 feed_dict=feed)
 
             end = time.time()
